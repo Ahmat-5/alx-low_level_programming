3b15bb703a689d25d396f450fa48550b66893708
Ahmat-5 <oumarissaahmat@gmail.com>
2023-07-17 15:21:58 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - prints "Programming is like gbuilding a multilingual puzze, followed by a newline"
+ * return 0.
+ */
+
+int main(void)
+{
+	puts("Programming is like gbuilding a multilingual puzze,");
+
+	return (0);
+}
